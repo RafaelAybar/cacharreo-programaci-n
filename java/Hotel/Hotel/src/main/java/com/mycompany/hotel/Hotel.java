b02ba20747e10c268533e091f0077027af2a0e3f
Rafael Aybar <rafaelaybar@hotmail.com>
2025-06-10 21:50:11 +0200

    Formateado código

--- java/Hotel/Hotel/src/main/java/com/mycompany/hotel/Hotel.java
+++ java/Hotel/Hotel/src/main/java/com/mycompany/hotel/Hotel.java
@@ -1,15 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.hotel;
+
 import java.util.Scanner;
+
 /**
  *
  * @author rafa
  */
 public class Hotel {
-/* Pendiente de generar el menú de opciones*/
+
+    /* Pendiente de generar el menú de opciones*/
     public static void main(String[] args) {
         System.out.println("Bienvenido al hotel continental \n Selecciona tu opción");
         System.out.println("""
@@ -18,19 +20,21 @@ public class Hotel {
                            3: Anular reserva
                            Cualquier otra tecla: Salir
                            """);
-        Scanner menu = new Scanner (System.in);
- 
+        Scanner menu = new Scanner(System.in);
+
         short respuesta = menu.nextShort();
-        switch (respuesta){
-            case 1: System.out.println("Introduce los datos");
-                break;
-            case 2: System.out.println("Introduce el identificador de reservas");
+        switch (respuesta) {
+            case 1 ->
+                System.out.println("Introduce los datos");
+            case 2 -> {
+                System.out.println("Introduce el identificador de reservas");
                 String id_reserva = menu.next();
                 System.out.println("La reserva a modificar es:" + id_reserva);
-                break;
-            case 3: System.out.println("Introduce el identificador de reservas para anular");
-                break;
-            default: System.out.println("Adios");
+            }
+            case 3 ->
+                System.out.println("Introduce el identificador de reservas para anular");
+            default ->
+                System.out.println("Adios");
         }
     }
 }
