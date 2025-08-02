eb217ccb2056e21868c73044c61a61766c75a89f
Rafael Aybar <rafaelaybar@hotmail.com>
2025-08-02 23:31:35 +0200

    Validada reserva

--- java/Hotel/Hotel/src/main/java/com/mycompany/hotel/Habitaciones.java
+++ java/Hotel/Hotel/src/main/java/com/mycompany/hotel/Habitaciones.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.hotel;
 
 /**
@@ -9,5 +5,6 @@ package com.mycompany.hotel;
  * @author rafa
  */
 public class Habitaciones {
-    
+    short numero_habitacion;
+    String id_reserva;
 }
